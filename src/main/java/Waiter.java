b8d6f651a5904f6a159e5f2f582a4100d559098b
ihor <ihprishchepa@gmail.com>
2023-12-12 17:14:15 +0200

    add test buy chocolateSanta

--- src/main/java/Waiter.java
+++ src/main/java/Waiter.java
@@ -1,12 +1,12 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import javax.swing.*;
 import java.time.Duration;
 import java.util.List;
 import java.util.NoSuchElementException;
